f000e79429a2be80b379e3301944f83ed4bf98bd
Sahib Dhanjal <dhanjalsahib@gmail.com>
2018-05-15 20:02:15 -0400

    Install script added

---
+++ install_astrobee.sh
@@ -0,0 +1,38 @@
+# export paths
+export SOURCE_PATH=$HOME/Documents/Astrobee/Freeflyer
+export ANDROID_PATH="${SOURCE_PATH}_android"
+export BUILD_PATH=$HOME/Documents/Astrobee/Freeflyer_build
+export INSTALL_PATH=$HOME/Documents/Astrobee/Freeflyer_install
+
+# clone repos
+git clone https://github.com/nasa/astrobee.git $SOURCE_PATH
+git clone https://github.com/nasa/astrobee_android.git $ANDROID_PATH
+
+# install dependencies
+pushd $SOURCE_PATH
+cd scripts/setup
+./add_ros_repository.sh
+cd debians
+./build_install_debians.sh
+cd ../
+./install_desktop_16_04_packages.sh
+sudo rosdep init
+rosdep update
+popd
+
+# Configure Build
+pushd $SOURCE_PATH
+./scripts/configure.sh -l -F -D -p $INSTALL_PATH -b $BUILD_PATH
+popd
+
+# Build the Code
+pushd $BUILD_PATH
+make -j6
+popd
+
+# Run simulation
+pushd $BUILD_PATH
+source devel/setup.bash
+popd
+
+roslaunch astrobee sim.launch dds:=false robot:=sim_pub rviz:=true
